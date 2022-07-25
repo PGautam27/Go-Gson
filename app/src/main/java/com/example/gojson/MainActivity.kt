4683148679e67c083578bb4c5fb3cecb8c339aa2
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-07-25 09:36:27 +0530

    Gson

--- app/src/main/java/com/example/gojson/MainActivity.kt
+++ app/src/main/java/com/example/gojson/MainActivity.kt
@@ -6,47 +6,66 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.gojson.ui.theme.GoJsonTheme
 import org.json.JSONObject
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GoJsonTheme {
-                val link = "https://api.coinpaprika.com/v1/coins"
+                val link = "https://random-data-api.com/api/blood/random_blood"
                 val jsonObject = JSONObject()
                 val apiLinkValue = remember {
                     mutableStateOf(TextFieldValue())
+                }
+                val jsonValue = remember {
+                    mutableStateOf(String())
                 }
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    OutlinedTextField(value = apiLinkValue.value, onValueChange = { apiLinkValue.value=it } , modifier = Modifier.width(
-                        LocalConfiguration.current.screenWidthDp.dp-100.dp), maxLines = 1)
+//                    OutlinedTextField(value = apiLinkValue.value, onValueChange = { apiLinkValue.value=it } , modifier = Modifier.width(
+//                        LocalConfiguration.current.screenWidthDp.dp-100.dp), maxLines = 1)
+                    Text(
+                        text = jsonValue.value
+                    )
                     Spacer(modifier = Modifier.padding(50.dp))
                     Button(onClick = {
-                        val que = Volley.newRequestQueue(this@MainActivity)
-                        val req = JsonObjectRequest(Request.Method.GET,link,jsonObject, {
-                            Toast.makeText(this@MainActivity,it.toString(),Toast.LENGTH_SHORT).show()
-                        }, {
-                            Toast.makeText(this@MainActivity,"Couldn't fetch",Toast.LENGTH_SHORT).show()
-                        })
+                        val queue = Volley.newRequestQueue(this@MainActivity)
+                        val url = "https://api.coinpaprika.com/v1/coins/btc-bitcoin"
+
+                        val stringRequest = StringRequest(
+                            Request.Method.GET, url,
+                            { response ->
+                                Toast.makeText(this@MainActivity,"Worked",Toast.LENGTH_SHORT).show()
+                                jsonValue.value = response
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Response: $response",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        ) {  Toast.makeText(
+                            applicationContext,
+                            "Response: It didn't work",
+                            Toast.LENGTH_SHORT
+                        ).show() }
+
+                        queue.add(stringRequest)
                     }) {
                         Text(text = "Click Me")
                     }
