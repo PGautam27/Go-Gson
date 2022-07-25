6253750b9c1138ee56238660f823aa7c41e7fe03
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-07-25 17:22:26 +0530

    Gson

--- app/src/main/java/com/example/gojson/MainActivity.kt
+++ app/src/main/java/com/example/gojson/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -16,6 +20,7 @@ import androidx.compose.ui.unit.dp
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.gojson.presentation.ViewModel
 import com.example.gojson.ui.theme.GoJsonTheme
 import org.json.JSONObject
 
@@ -26,46 +31,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             GoJsonTheme {
                 val link = "https://random-data-api.com/api/blood/random_blood"
-                val jsonObject = JSONObject()
+                val viewModel = ViewModel()
                 val apiLinkValue = remember {
                     mutableStateOf(TextFieldValue())
                 }
-                val jsonValue = remember {
-                    mutableStateOf(String())
-                }
+                val jsonValue = viewModel.jsonData.observeAsState().value.toString()
                 Column(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState()),
                     verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-//                    OutlinedTextField(value = apiLinkValue.value, onValueChange = { apiLinkValue.value=it } , modifier = Modifier.width(
-//                        LocalConfiguration.current.screenWidthDp.dp-100.dp), maxLines = 1)
+                    OutlinedTextField(value = apiLinkValue.value, onValueChange = { apiLinkValue.value=it } , modifier = Modifier.width(
+                        LocalConfiguration.current.screenWidthDp.dp-100.dp), maxLines = 1)
                     Text(
-                        text = jsonValue.value
+                        text = jsonValue
                     )
                     Spacer(modifier = Modifier.padding(50.dp))
                     Button(onClick = {
-                        val queue = Volley.newRequestQueue(this@MainActivity)
-                        val url = "https://api.coinpaprika.com/v1/coins/btc-bitcoin"
-
-                        val stringRequest = StringRequest(
-                            Request.Method.GET, url,
-                            { response ->
-                                Toast.makeText(this@MainActivity,"Worked",Toast.LENGTH_SHORT).show()
-                                jsonValue.value = response
-                                Toast.makeText(
-                                    applicationContext,
-                                    "Response: $response",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        ) {  Toast.makeText(
-                            applicationContext,
-                            "Response: It didn't work",
-                            Toast.LENGTH_SHORT
-                        ).show() }
-
-                        queue.add(stringRequest)
+                        viewModel.fetchLink(apiLinkValue.value.text,this@MainActivity)
                     }) {
                         Text(text = "Click Me")
                     }
@@ -74,3 +59,25 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+//
+//val queue = Volley.newRequestQueue(this@MainActivity)
+//val url = "https://api.coinpaprika.com/v1/coins/btc-bitcoin"
+//
+//val stringRequest = StringRequest(
+//    Request.Method.GET, apiLinkValue.value.text,
+//    { response ->
+//        Toast.makeText(this@MainActivity,"Worked",Toast.LENGTH_SHORT).show()
+//        jsonValue.value = response
+//        Toast.makeText(
+//            applicationContext,
+//            "Response: $response",
+//            Toast.LENGTH_SHORT
+//        ).show()
+//    }
+//) {  Toast.makeText(
+//    applicationContext,
+//    "Response: It didn't work",
+//    Toast.LENGTH_SHORT
+//).show() }
+//
+//queue.add(stringRequest)
--- app/src/main/java/com/example/gojson/presentation/ViewModel.kt
+++ app/src/main/java/com/example/gojson/presentation/ViewModel.kt
@@ -1,4 +1,43 @@
 package com.example.gojson.presentation
 
-class ViewModel {
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class ViewModel():ViewModel() {
+
+    private val link = MutableLiveData<String>()
+
+    private val _jsonData = MutableLiveData<String>()
+    val jsonData:LiveData<String> = _jsonData
+
+    fun fetchLink(activeLink : String, context:ComponentActivity){
+        link.value = activeLink
+        convertJsonData(context)
+    }
+
+    private fun convertJsonData(context: ComponentActivity){
+        val queue = Volley.newRequestQueue(context)
+
+        val stringRequest = StringRequest(
+            Request.Method.GET, link.value.toString(),
+            { response ->
+                _jsonData.value = response
+            }
+        ) {
+            Toast.makeText(
+                context,
+                "Response: It didn't work",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        queue.add(stringRequest)
+    }
+
 }
