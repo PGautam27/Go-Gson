fd145c389b69f69168d9ee2b3f45d0c28543224f
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-07-26 08:54:57 +0530

    Gson

--- app/src/main/java/com/example/gojson/presentation/JsonDataScreen.kt
+++ app/src/main/java/com/example/gojson/presentation/JsonDataScreen.kt
@@ -1,5 +1,10 @@
 package com.example.gojson.presentation
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -10,10 +15,13 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.PermissionChecker.checkSelfPermission
 
 @Composable
-fun JsonDataScreen(viewModel: ViewModel) {
+fun JsonDataScreen(viewModel: ViewModel,context: ComponentActivity) {
     val jsonData = viewModel.jsonData.observeAsState().value.toString()
+    val STORAGE_PERMISSION_CODE = 1000
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -23,9 +31,25 @@ fun JsonDataScreen(viewModel: ViewModel) {
     ) {
         Text(text = jsonData)
         Spacer(modifier = Modifier.padding(20.dp))
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(context,Manifest.permission.WRITE_EXTERNAL_STORAGE)==
+                        PackageManager.PERMISSION_DENIED){
+                    requestPermissions(context,arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),STORAGE_PERMISSION_CODE)
+                } else{
+                    startDownloading()
+                }
+            }
+            else{
+                startDownloading()
+            }
+        }) {
             Text(text = "Download file.txt")
         }
     }
 }
 
+
+fun startDownloading(){
+
+}
